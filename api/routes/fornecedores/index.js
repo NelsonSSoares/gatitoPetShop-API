6bd348ed651598793bed10d5b51a872c5666e311
Nelson <punkpunkada@gmail.com>
2022-02-03 17:24:15 -0300

    database settings

--- api/routes/fornecedores/index.js
+++ api/routes/fornecedores/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const TabelaFornecedor = require('./TabelaFornecedor')
 
 router.use('/', async (req, res)=> {
+    
     const response = await TabelaFornecedor.listar()
     res.send(JSON.stringify(response))
 })
